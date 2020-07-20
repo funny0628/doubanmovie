667752fad9057bbc9e318d71d871a10270b0aaee
funny0628 <610531115@qq.com>
2020-07-20 16:53:53 +0800

    first commit

---
+++ src/pages/movedetail/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+
+
+  data () {
+    return {
+     
+    }
+  },
+
+  created () {
+  
+  }
+}
+</script>
+
+<style>
+.log-list {
+  display: flex;
+  flex-direction: column;
+  padding: 40rpx;
+}
+
+.log-item {
+  margin: 10rpx;
+}
+</style>
---
+++ src/pages/moveindex/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="moveindex">
+    <div v-if="false" class="topbar">
+      <div class="left">
+        <span>豆瓣</span>
+        <i></i>
+      </div>
+      <div class="right">打开豆瓣App</div>
+    </div>
+    <div class="container">
+      <p class="title">最热销的</p>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+     
+    }
+  },
+
+
+
+  methods: {
+   
+  },
+
+  created () {
+
+  }
+}
+</script>
+
+<style scoped>
+.moveindex {
+  height: 100vh;
+  background-color: pink;
+}
+
+</style>
